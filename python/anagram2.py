cb6b88a40d95ecd445fad2d78f8b3b2a3ef76a81
meredithhall716 <meredith.hall.oboe@gmail.com>
2022-05-28 14:22:37 -0400

    first

--- python/anagram2.py
+++ python/anagram2.py
@@ -1,4 +1,16 @@
 # Don't forget to run the tests (and create some of your own)
 
+from multiprocessing.connection import answer_challenge
+
+
 def anagrams_for(word, list_of_words):
-		# your code here
+		sorted_word = sorted(word)
+		ans = []
+		for x in list_of_words:
+			sorted_elem = sorted(x)
+
+			if sorted_elem == sorted_word:
+				ans.append(x)
+		
+		return ans
+	
